d65636075c8810e00a58d17bcb7e0a5d2a8789b0
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2023-12-07 17:30:39 +0300

    feat: add manifest module and test

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
-import { ManifestService } from './manifest/manifest.service';
+import { ManifestModule } from './manifest/manifest.module';
 
 @Module({
-  imports: [],
+  imports: [ManifestModule],
   controllers: [ApiController],
-  providers: [ApiService, ManifestService],
+  providers: [ApiService],
 })
 export class ApiModule {}
