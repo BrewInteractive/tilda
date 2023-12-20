f52fcef3b88896a7ee1382f1dca8175f8d6d400e
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2023-12-20 14:25:42 +0300

    feat: create manifest service validate manifest

---
+++ apps/api/src/manifest/fixtures/manifest-schema-test.ts
@@ -0,0 +1,253 @@
+import { TildaManifest } from '../../models';
+
+export const validManifest = {
+  hmac: '',
+  data: {
+    fields: {
+      name: {
+        inputName: '',
+        ui: {
+          label: 'Name',
+        },
+        const: {
+          constName1: 'const value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+      surname: {
+        inputName: '',
+        ui: {
+          label: 'Surname',
+        },
+        const: {
+          'constName2:enc': 'encrypted value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+    },
+    hooks: {
+      pre: [
+        {
+          factory: 'webhook',
+          params: {
+            url: 'test.example.com',
+            headers: [],
+            method: 'post',
+            values: {
+              nameSurname: '{$.fields.name.value} {$.fields.surname.value}',
+            },
+          },
+        },
+      ],
+      post: [
+        {
+          factory: 'email',
+          params: {
+            recipients: [
+              {
+                'email:enc': 'encrypted email address',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+} as TildaManifest;
+export const requiredFieldMissingManifest = {
+  hmac: '',
+  data: {
+    fields: {},
+    hooks: {
+      pre: [
+        {
+          factory: 'webhook',
+          params: {
+            url: 'test.example.com',
+            headers: [],
+            method: 'post',
+            values: {
+              nameSurname: '{$.fields.name.value} {$.fields.surname.value}',
+            },
+          },
+        },
+      ],
+      post: [
+        {
+          factory: 'email',
+          params: {
+            recipients: [
+              {
+                'email:enc': 'encrypted email address',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+} as TildaManifest;
+export const requiredHookMissingManifest = {
+  hmac: '',
+  data: {
+    fields: {
+      name: {
+        inputName: '',
+        ui: {
+          label: 'Name',
+        },
+        const: {
+          constName1: 'const value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+      surname: {
+        inputName: '',
+        ui: {
+          label: 'Surname',
+        },
+        const: {
+          'constName2:enc': 'encrypted value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+    },
+    hooks: {},
+  },
+} as TildaManifest;
+export const requiredPreHookParamsForMissingUrl = {
+  hmac: '',
+  data: {
+    fields: {
+      name: {
+        inputName: '',
+        ui: {
+          label: 'Name',
+        },
+        const: {
+          constName1: 'const value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+      surname: {
+        inputName: '',
+        ui: {
+          label: 'Surname',
+        },
+        const: {
+          'constName2:enc': 'encrypted value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+    },
+    hooks: {
+      pre: [
+        {
+          factory: 'webhook',
+          params: {
+            url: '',
+            headers: [],
+            method: 'post',
+            values: {
+              nameSurname: '{$.fields.name.value} {$.fields.surname.value}',
+            },
+          },
+        },
+      ],
+      post: [
+        {
+          factory: 'email',
+          params: {
+            recipients: [
+              {
+                'email:enc': 'encrypted email address',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+} as TildaManifest;
+export const requiredPostHookParamsForMissingEmail = {
+  hmac: '',
+  data: {
+    fields: {
+      name: {
+        inputName: '',
+        ui: {
+          label: 'Name',
+        },
+        const: {
+          constName1: 'const value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+      surname: {
+        inputName: '',
+        ui: {
+          label: 'Surname',
+        },
+        const: {
+          'constName2:enc': 'encrypted value',
+        },
+        validators: [
+          {
+            factory: 'numeric',
+          },
+        ],
+      },
+    },
+    hooks: {
+      pre: [
+        {
+          factory: 'webhook',
+          params: {
+            url: 'test.example.com',
+            headers: [],
+            method: 'post',
+            values: {
+              nameSurname: '{$.fields.name.value} {$.fields.surname.value}',
+            },
+          },
+        },
+      ],
+      post: [
+        {
+          factory: 'email',
+          params: {
+            recipients: null,
+          },
+        },
+      ],
+    },
+  },
+} as TildaManifest;
