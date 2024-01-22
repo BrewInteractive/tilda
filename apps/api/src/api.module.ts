9fa7eccb70dd2ac5efefeb5fd589cd655f4afd40
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2024-01-22 11:44:31 +0300

    feat: create bull queue

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -4,10 +4,12 @@ import { ApiService } from './api.service';
 import { ManifestModule } from './manifest/manifest.module';
 import { ConfigModule } from '@nestjs/config';
 import config from '../src/config/configuration';
+import { BullQueueModule } from './queue/bull.module';
 
 @Module({
   imports: [
     ManifestModule,
+    BullQueueModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -1,4 +1,6 @@
 export default () => ({
   ENCRYPTION_SECRET: process.env.ENCRYPTION_SECRET,
   SWAGGER_ENABLED: process.env.SWAGGER_ENABLED === 'true' || false,
+  BULL_HOST: process.env.BULL_HOST,
+  BULL_PORT: process.env.BULL_PORT,
 });
