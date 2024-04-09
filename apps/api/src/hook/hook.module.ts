04c76a9571556c0a473ce3608657896ef126a21f
Ali Torun <toruna@gmail.com>
2024-04-09 09:38:59 +0300

    Add DataCordProcessor to HookModule providers

--- apps/api/src/hook/hook.module.ts
+++ apps/api/src/hook/hook.module.ts
@@ -1,4 +1,5 @@
 import { ConfigModule } from '@nestjs/config';
+import { DataCordProcessor } from './processors/datacord.processor';
 import { EmailModule } from '../email/email.module';
 import { EmailProcessor } from './processors/email.processor';
 import { HookProcessorFactory } from './hook.factory';
@@ -7,7 +8,12 @@ import { WebhookProcessor } from './processors/webhook.processor';
 
 @Module({
   imports: [EmailModule, ConfigModule],
-  providers: [EmailProcessor, WebhookProcessor, HookProcessorFactory],
+  providers: [
+    EmailProcessor,
+    WebhookProcessor,
+    DataCordProcessor,
+    HookProcessorFactory,
+  ],
   exports: [HookProcessorFactory],
 })
 export class HookModule {}
